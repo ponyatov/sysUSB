880a1c8392b5c12d58931289df3087e9e09bb10e
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-30 22:51:12 +0400

    * memdisk + ls
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- win32_mksysdir.bat
+++ win32_mksysdir.bat
@@ -4,6 +4,9 @@ unzip -jn syslinux-6.03.zip bios\win32\syslinux.exe -d syslinux
 unzip -jn syslinux-6.03.zip bios\com32\lib\libcom32.c32 -d syslinux
 unzip -jn syslinux-6.03.zip bios\com32\libutil\libutil.c32 -d syslinux
 unzip -jn syslinux-6.03.zip bios\com32\menu\menu.c32 -d syslinux
+unzip -jn syslinux-6.03.zip bios\memdisk\memdisk -d syslinux
+unzip -jn syslinux-6.03.zip bios\com32\modules\ls.c32 -d syslinux
+
 rem unzip -jn syslinux-6.03.zip bios\com32\menu\vesamenu.c32 -d syslinux
 rem unzip -jn syslinux-6.03.zip bios\com32\chain\chain.c32 -d syslinux
 
@@ -12,6 +15,7 @@ cp *.exe syslinux/
 cp memtest.krn syslinux/
 cp *.cfg syslinux/
 cp README.md syslinux/
+cp syslinux-6.03.zip syslinux/
 
 pause
 
